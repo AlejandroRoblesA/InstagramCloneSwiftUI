7060b8f694e217f67d4bc2a2003612e523420310
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-15 13:25:03 -0600

    TabView items

---
+++ InstagramCloneSwiftUI/Core/TabBar/MainTabView.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabView.swift
+//  InstagramCloneSwiftUI
+//
+//  Created by Alejandro Robles on 15/06/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
