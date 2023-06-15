1e3f8611461909ffc664adbb7c3e2ea66e771e0b
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-14 19:47:00 -0600

    Project created

---
+++ InstagramCloneSwiftUI/InstagramCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneSwiftUIApp.swift
+//  InstagramCloneSwiftUI
+//
+//  Created by Alejandro Robles on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
