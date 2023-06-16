830f714d68bffb4fd4b76d11f73c8bc485da8d01
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-16 13:39:42 -0600

    NavigationBar customized

--- InstagramCloneSwiftUI/Core/Search/SearchView.swift
+++ InstagramCloneSwiftUI/Core/Search/SearchView.swift
@@ -8,28 +8,35 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 12) {
-                ForEach(0...15, id: \.self) { _  in
-                    HStack {
-                        Image("batman2")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 40, height: 40)
-                            .clipShape(Circle())
-                        VStack(alignment: .leading) {
-                            Text("batman")
-                                .fontWeight(.semibold)
-                            Text("Bruce Wayne")
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0...5, id: \.self) { _  in
+                        HStack {
+                            Image("batman2")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            VStack(alignment: .leading) {
+                                Text("batman")
+                                    .fontWeight(.semibold)
+                                Text("Bruce Wayne")
+                            }
+                            .font(.footnote)
+                            Spacer()
                         }
-                        .font(.footnote)
-                        Spacer()
+                        .padding(.horizontal)
+                        
                     }
-                    .padding(.horizontal)
-                    
                 }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
             }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
