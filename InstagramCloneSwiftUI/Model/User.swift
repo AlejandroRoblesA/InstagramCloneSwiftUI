4aa733fba50ca270ffd0d4dfdf0f73d46dbfbf25
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-18 13:29:39 -0600

    User extension

--- InstagramCloneSwiftUI/Model/User.swift
+++ InstagramCloneSwiftUI/Model/User.swift
@@ -15,3 +15,13 @@ struct User: Identifiable, Codable {
     var bio: String?
     let email: String
 }
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: NSUUID().uuidString, userName: "batman", profileImageUrl: "batman", fullName: "Bruce Wayne", bio: "Gotham's Dark Knight", email: "batman@gmail.com"),
+        .init(id: NSUUID().uuidString, userName: "vemom", profileImageUrl: "venom", fullName: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
+        .init(id: NSUUID().uuidString, userName: "ironman", profileImageUrl: "ironman", fullName: "Tony Stark", bio: "Iron Man", email: "ironman@gmail.com"),
+        .init(id: NSUUID().uuidString, userName: "blackpanther", profileImageUrl: "blackpanther", fullName: "Chadwick Boseman", bio: "Black Panther", email: "blackpanther@gmail.com"),
+        .init(id: NSUUID().uuidString, userName: "spiderman", profileImageUrl: "spiderman", fullName: "Peter Parker", bio: "Spiderman", email: "spiderman@gmail.com")
+    ]
+}
