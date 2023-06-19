355c1e7eb24646f7463b4ebcbce787225c5274cf
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-19 13:44:19 -0600

    Post extension data

--- InstagramCloneSwiftUI/Model/Post.swift
+++ InstagramCloneSwiftUI/Model/Post.swift
@@ -16,3 +16,36 @@ struct Post: Identifiable, Hashable, Codable {
     let timestamp: Date
     var user: User?
 }
+
+extension Post {
+    static var MOCK_POSTS: [Post] = [
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "This is some test caption for now",
+              likes: 123,
+              imageURL: "batman",
+              timestamp: Date(),
+              user: User.MOCK_USERS[0]),
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "Wakanda Forever",
+              likes: 104,
+              imageURL: "blackpanther",
+              timestamp: Date(),
+              user: User.MOCK_USERS[3]),
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "Iron Man",
+              likes: 12,
+              imageURL: "ironman",
+              timestamp: Date(),
+              user: User.MOCK_USERS[2]),
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "Venom is hungry. Time to eat",
+              likes: 123,
+              imageURL: "venom2",
+              timestamp: Date(),
+              user: User.MOCK_USERS[1])
+    ]
+}
