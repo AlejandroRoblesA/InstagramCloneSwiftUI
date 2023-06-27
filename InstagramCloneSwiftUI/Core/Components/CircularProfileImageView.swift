e05948e8addfd1647bd2040a95bc1d1aba65da86
Alejandro Robles <alejandro_r1704@hotmail.com>
2023-06-27 12:50:37 -0600

    CircularProfile ImageView file

---
+++ InstagramCloneSwiftUI/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,34 @@
+//
+//  CircularProfileImageView.swift
+//  InstagramCloneSwiftUI
+//
+//  Created by Alejandro Robles on 27/06/23.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct CircularProfileImageView: View {
+    let user: User
+    
+    var body: some View {
+        if let imageUrl = user.profileImageUrl {
+            KFImage(URL(string: imageUrl))
+                .resizable()
+                .scaledToFill()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+        } else {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+        }
+    }
+}
+
+struct CircularProfileImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProfileImageView(user: User.MOCK_USERS[2])
+    }
+}
